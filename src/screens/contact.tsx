7b5c17e3890fc36e8f96b5de240dcd4bc8f7f161
valentin <valentin.minkov@catenate.com>
2019-07-08 16:33:55 +0200

    contact added

--- src/screens/contact.tsx
+++ src/screens/contact.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from '../components/UI/header/header'
-
+import './contact.scss'
 class ContactScreen extends React.Component {
 	render() {
 		return (
 			<div className="aboutscreen-container">
 				<Header content="contact" />
+
+				<div className='content-container mail-container'>
+				<i className="material-icons">mail_outline</i> <span>minkowalentin@gmail.com</span>
+				</div>
 			</div>
 		)
 	}
